34c9bac0bb64d7791dcdf634ecaf576c9d161a5c
ar-kun <ar-kun@users.noreply.github.com>
2025-07-22 14:46:44 +0000

    Fix styling (php-cs-fixer & blade formatter github actions)

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,4 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
