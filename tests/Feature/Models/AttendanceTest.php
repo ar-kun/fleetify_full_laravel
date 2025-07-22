7aae092d0ea121d8c0ba866e8bf2e9e95071937e
Adam Ramdan <79040246+ar-kun@users.noreply.github.com>
2025-07-22 23:31:05 +0700

    feat: add AttendanceTest to validate attendance relationships with employee and attendance histories

---
+++ tests/Feature/Models/AttendanceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Attendance;
+use App\Models\AttendanceHistory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AttendanceTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * A test to check relationship attendance belongs to an employee.
+     */
+    public function test_attendance_belongs_to_employee(): void
+    {
+        $attendance = Attendance::factory()->create();
+        $this->assertTrue($attendance->employee->is($attendance->employee));
+    }
+
+    /**
+     * A test to check relationship attendance has many attendance histories.
+     */
+    public function test_attendance_has_many_attendance_histories(): void
+    {
+        $attendance = Attendance::factory()->create();
+        $attendanceHistories = AttendanceHistory::factory()->count(3)->create(['attendance_id' => $attendance->attendance_id]);
+
+        $relatedAttendanceHistories = $attendance->attendanceHistories;
+        $this->assertCount(3, $relatedAttendanceHistories);
+        $this->assertTrue($relatedAttendanceHistories->contains($attendanceHistories->first()));
+    }
+}
