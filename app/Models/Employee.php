954470ce078d41a1cafff454e183c24a0f09d042
Adam Ramdan <79040246+ar-kun@users.noreply.github.com>
2025-07-22 22:56:45 +0700

    feat: enhance model definitions with relationships and fillable properties

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -1,12 +1,59 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Employee
+ * 
+ * @property int $id
+ * @property string $employee_id
+ * @property int $department_id
+ * @property string $name
+ * @property string $address
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * 
+ * @property Department $department
+ * @property Collection|AttendanceHistory[] $attendance_histories
+ * @property Collection|Attendance[] $attendances
+ *
+ * @package App\Models
+ */
 class Employee extends Model
 {
-    /** @use HasFactory<\Database\Factories\EmployeeFactory> */
-    use HasFactory;
+	protected $table = 'employees';
+
+	protected $casts = [
+		'department_id' => 'int'
+	];
+
+	protected $fillable = [
+		'employee_id',
+		'department_id',
+		'name',
+		'address'
+	];
+
+	public function department()
+	{
+		return $this->belongsTo(Department::class);
+	}
+
+	public function attendanceHistories()
+	{
+		return $this->hasMany(AttendanceHistory::class, 'employee_id', 'employee_id');
+	}
+
+	public function attendances()
+	{
+		return $this->hasMany(Attendance::class, 'employee_id', 'employee_id');
+	}
 }
