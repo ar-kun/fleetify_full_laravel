d117133043cc0023d17ae26727830bd7f655136b
ar-kun <ar-kun@users.noreply.github.com>
2025-07-22 16:33:35 +0000

    Fix styling (php-cs-fixer & blade formatter github actions)

--- app/Models/AttendanceHistory.php
+++ app/Models/AttendanceHistory.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class AttendanceHistory
- * 
+ *
  * @property int $id
  * @property string $employee_id
  * @property string $attendance_id
@@ -20,37 +20,34 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $attendance_type
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * 
  * @property Attendance $attendance
  * @property Employee $employee
- *
- * @package App\Models
  */
 class AttendanceHistory extends Model
 {
-	use HasFactory;
+    use HasFactory;
 
-	protected $table = 'attendance_histories';
+    protected $table = 'attendance_histories';
 
-	protected $casts = [
-		'date_attendance' => 'datetime',
-		'attendance_type' => 'int'
-	];
+    protected $casts = [
+        'date_attendance' => 'datetime',
+        'attendance_type' => 'int',
+    ];
 
-	protected $fillable = [
-		'employee_id',
-		'attendance_id',
-		'date_attendance',
-		'attendance_type'
-	];
+    protected $fillable = [
+        'employee_id',
+        'attendance_id',
+        'date_attendance',
+        'attendance_type',
+    ];
 
-	public function attendance()
-	{
-		return $this->belongsTo(Attendance::class, 'attendance_id', 'attendance_id');
-	}
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class, 'attendance_id', 'attendance_id');
+    }
 
-	public function employee()
-	{
-		return $this->belongsTo(Employee::class, 'employee_id', 'employee_id');
-	}
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'employee_id', 'employee_id');
+    }
 }
