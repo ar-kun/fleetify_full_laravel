10c28128a7ca2e408129a902fa362dee28e08fff
Adam Ramdan <79040246+ar-kun@users.noreply.github.com>
2025-07-23 14:22:03 +0700

    feat: implement employee listing with pagination and create view

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -12,7 +12,8 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+        $employees = Employee::with('department')->paginate(5);
+        return view('employees.index', compact('employees'));
     }
 
     /**
