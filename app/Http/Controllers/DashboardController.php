e0c1e24dce4620a12afa2dd875be821393f18cbd
Adam Ramdan <79040246+ar-kun@users.noreply.github.com>
2025-07-23 20:35:49 +0700

    feat: enhance dashboard with attendance history and update view logic

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attendance;
+use App\Models\AttendanceHistory;
 
 class DashboardController extends Controller
 {
@@ -13,6 +14,10 @@ class DashboardController extends Controller
             ->latest()
             ->first();
 
-        return view('dashboard', compact('attendance'));
+        $attendanceHistory = AttendanceHistory::with('employee')->where('employee_id', auth()->user()->employee_id)
+            ->latest()->limit(6)
+            ->get();
+
+        return view('dashboard', compact('attendance', 'attendanceHistory'));
     }
 }
